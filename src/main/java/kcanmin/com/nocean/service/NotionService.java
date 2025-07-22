44f30e46dd2d371c28adf03c35a4433cc1ef2217
DaHnDell <khanman1208@gmail.com>
2025-07-22 19:30:20 +0900

    NotionService prototype add

---
+++ src/main/java/kcanmin/com/nocean/service/NotionService.java
@@ -0,0 +1,9 @@
+package kcanmin.com.nocean.service;
+
+import org.springframework.stereotype.Service;
+
+public interface NotionService {
+
+  void record(String message, String summary, String url);
+
+}
---
+++ src/main/java/kcanmin/com/nocean/service/NotionServiceImpl.java
@@ -0,0 +1,10 @@
+package kcanmin.com.nocean.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotionServiceImpl implements NotionService {
+  public void record(String message, String summary, String url){
+
+  }
+}
