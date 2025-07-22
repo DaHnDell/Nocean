a17b4e0b4b35c22d932cfec0cf02840bb5f635ee
DaHnDell <khanman1208@gmail.com>
2025-07-22 18:10:31 +0900

    project init - SpringBoot 3.5.3

---
+++ src/main/java/kcanmin/com/nocean/NoceanApplication.java
@@ -0,0 +1,13 @@
+package kcanmin.com.nocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoceanApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NoceanApplication.class, args);
+  }
+
+}
---
+++ src/test/java/kcanmin/com/nocean/NoceanApplicationTests.java
@@ -0,0 +1,13 @@
+package kcanmin.com.nocean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoceanApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
