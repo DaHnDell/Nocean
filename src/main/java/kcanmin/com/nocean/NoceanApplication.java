996f09764b7688d2bece072b51095faccfeae7ab
DaHnDell <khanman1208@gmail.com>
2025-07-23 14:34:34 +0900

    main app mod

--- src/main/java/kcanmin/com/nocean/NoceanApplication.java
+++ src/main/java/kcanmin/com/nocean/NoceanApplication.java
@@ -2,8 +2,10 @@ package kcanmin.com.nocean;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class NoceanApplication {
 
   public static void main(String[] args) {
